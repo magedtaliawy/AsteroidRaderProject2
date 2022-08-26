891b904f364e083c9e9314bf74a810a5577bd4cf
maged <maged.taliawy@gmail.com>
2022-08-26 20:41:51 +0200

    init commit

--- app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -11,7 +11,7 @@ import com.udacity.asteroidradar.main.AsteroidAdapter
 fun loadImage(imageView: ImageView, pictureOfDay: PictureOfDay?) {
     if (pictureOfDay?.mediaType == "image") {
         pictureOfDay.url?.let {
-            Picasso.with(imageView.context).load(pictureOfDay.url).fit().resize(200, 200)
+            Picasso.with(imageView.context).load(pictureOfDay.url).fit()
                 .centerCrop()
                 .into(imageView)
         }
@@ -22,8 +22,10 @@ fun loadImage(imageView: ImageView, pictureOfDay: PictureOfDay?) {
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.ic_status_potentially_hazardous)
+        imageView.contentDescription=imageView.context.getString(R.string.potentially_hazardous_asteroid_image)
     } else {
         imageView.setImageResource(R.drawable.ic_status_normal)
+        imageView.contentDescription=imageView.context.getString(R.string.normal_asteroid_image)
     }
 }
 
@@ -31,8 +33,10 @@ fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
+        imageView.contentDescription=imageView.context.getString(R.string.hazardous_asteroid_image)
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
+        imageView.contentDescription=imageView.context.getString(R.string.not_hazardous_asteroid_image)
     }
 }
 
